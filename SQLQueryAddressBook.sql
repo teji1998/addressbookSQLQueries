07df51a1d5e08f6af8b339e577f74f7813b7bddb
“teji1998” <tejasvinirpk@gmail.com>
2020-12-15 16:11:43 +0530

    Added:Code to create a table in database

--- SQLQueryAddressBook.sql
+++ SQLQueryAddressBook.sql
@@ -3,3 +3,11 @@
 create database Address_Book_Service;
 --using the database
 use Address_Book_Service;
+/*UC2*/
+--creating address book table
+Create table Address_Book(Id int identity(1,1) primary key,First_Name Varchar(30),Last_Name Varchar(30),
+Address Varchar(50),City Varchar(50),State Varchar(50),Zip int,Mobile_Number Varchar(10) , 
+Email_Id Varchar(40) ) ;
+/*--To change datatype of mobile number from varchar to int
+Alter Table Address_Book
+Alter Column Mobile_Number int;*/
