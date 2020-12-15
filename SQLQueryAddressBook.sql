7b477ee6277527c2e6affca30115212f8eb8111a
“teji1998” <tejasvinirpk@gmail.com>
2020-12-15 16:21:32 +0530

    Refactored:Changed query to make mobile number and emailId unique

--- SQLQueryAddressBook.sql
+++ SQLQueryAddressBook.sql
@@ -6,8 +6,8 @@ use Address_Book_Service;
 /*UC2*/
 --creating address book table
 Create table Address_Book(Id int identity(1,1) primary key,First_Name Varchar(30),Last_Name Varchar(30),
-Address Varchar(50),City Varchar(50),State Varchar(50),Zip int,Mobile_Number Varchar(10) , 
-Email_Id Varchar(40) ) ;
+Address Varchar(50),City Varchar(50),State Varchar(50),Zip int,Mobile_Number Varchar(10) unique, 
+Email_Id Varchar(40) unique ) ;
 /*--To change datatype of mobile number from varchar to int
 Alter Table Address_Book
 Alter Column Mobile_Number int;*/
