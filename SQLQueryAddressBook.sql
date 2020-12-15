92a44beb43f5e3956a709e924d506ffdede4d7c1
“teji1998” <tejasvinirpk@gmail.com>
2020-12-15 16:31:44 +0530

    Added:Query to insert records into table

--- SQLQueryAddressBook.sql
+++ SQLQueryAddressBook.sql
@@ -7,7 +7,25 @@ use Address_Book_Service;
 --creating address book table
 Create table Address_Book(Id int identity(1,1) primary key,First_Name Varchar(30),Last_Name Varchar(30),
 Address Varchar(50),City Varchar(50),State Varchar(50),Zip int,Mobile_Number Varchar(10) unique, 
-Email_Id Varchar(40) unique ) ;
+Email_Id Varchar(40) unique) ;
 /*--To change datatype of mobile number from varchar to int
 Alter Table Address_Book
 Alter Column Mobile_Number int;*/
+/*UC3*/
+--To insert new Contact into addressbook
+insert into Address_Book(First_Name,Last_Name,Address,City,State,Zip,Mobile_Number,Email_Id)
+values('Tejaswini','Kulkarni','Chembur','Mumbai','Maharashtra','400071','9920275347','tejasvinirpk@gmail.com');
+insert into Address_Book(First_Name,Last_Name,Address,City,State,Zip,Mobile_Number,Email_Id)
+values('Lee','Minho','Palace','Seoul','Korea','898976','6768995434','lee_minho@kbstv.com');
+insert into Address_Book(First_Name,Last_Name,Address,City,State,Zip,Mobile_Number,Email_Id)
+values('Aryan','Pai','Blueivy','Bangalore','Karnataka','560878','9845622100','arayan148@abc.com');
+insert into Address_Book(First_Name,Last_Name,Address,City,State,Zip,Mobile_Number,Email_Id)
+values('Varun','Exo','Fatorda','Ponda','Goa','458678','8786554400','varun_exo@sbs.com');
+insert into Address_Book(First_Name,Last_Name,Address,City,State,Zip,Mobile_Number,Email_Id)
+values('Tejaswini','Shetti','Dharwad','Marines','Konkan','858678','8786499400','teju_sj13@sj.com');
+insert into Address_Book(First_Name,Last_Name,Address,City,State,Zip,Mobile_Number,Email_Id)
+values('Roopa','Rajkumar','Kusugal','Hubli','Karnataka','563222','9920307457','roopa_brt@yahoo.com');
+insert into Address_Book(First_Name,Last_Name,Address,City,State,Zip,Mobile_Number,Email_Id)
+values('Aryan','Wellington','lakeview','Hubli','Karnataka','560788','8777622100','arayanwelli@abdc.com');
+insert into Address_Book(First_Name,Last_Name,Address,City,State,Zip,Mobile_Number,Email_Id)
+values('Abhimanyu','Jagirdar','Farmhouse','Mumbai','Maharashtra','400767','8956434789','abhi_jagir@cmtv.com');
